54db686f76ca36b14d21e9cb336c06ab41af2c7a
grassele <91402260+grassele@users.noreply.github.com>
2023-02-07 19:48:29 -0800

    renewed execlp attempt

--- redirout.c
+++ redirout.c
@@ -15,7 +15,13 @@ int main(int argc, char *argv[])
     int fd;
     fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
     dup2(fd, 1);
-    execlp(argv[2], *argv);
+
+    char *com_args[10];
+    for (int i = 2; i < argc; i++) {
+        com_args[i] = argv[i];
+    }
+    
+    execlp(com_args, NULL);
 
 
 }
